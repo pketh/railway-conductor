dccd35adfa76876cc16148d680c3db08ef45b5a4
Pirijan <hi@pirijan.com>
2025-05-09 14:14:39 -0400

    add updateActionButton

--- public/index.js
+++ public/index.js
@@ -1,8 +1,28 @@
 const updateStatusElement = ({ isLoading, isRunning, isStopped, isUnknown }) => {
   const element = document.querySelector('.status-badge')
   if (isRunning) {
-    element.classList.add('success')
+    element.classList.add('running')
     element.innerText = 'Running'
+  } else if (isLoading) {
+    element.classList.add('loading')
+    element.innerText = 'Loading'
+  } else if (isStopped) {
+    element.classList.add('stopped')
+    element.innerText = 'Stopped'
+  } else if (isLoading) {
+    element.classList.add('unknown')
+    element.innerText = 'Could not determine status'
+  }
+}
+
+const updateActionButton = ({ isLoading, isRunning, isStopped, isUnknown }) => {
+  const element = document.querySelector('.action-button')
+  if (isRunning) {
+    element.classList.add('stop')
+    element.innerText = 'stop'
+  } else {
+    element.classList.add('play')
+    element.innerText = 'play'
   }
 }
 
@@ -22,7 +42,7 @@ const updateStatus = async () => {
     const isUnknown = status === 'UNKNOWN'
     console.log('🔮', status, { isLoading, isRunning, isStopped, isUnknown })
     updateStatusElement({ isLoading, isRunning, isStopped, isUnknown })
-    // updateActionButton({ isLoading, isRunning, isStopped, isUnknown })
+    updateActionButton({ isLoading, isRunning, isStopped, isUnknown })
   } catch (error) {
     console.error('🚒 status', error)
     updateStatusElement({ isUnknown: true })
