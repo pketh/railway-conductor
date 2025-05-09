c41181ed84ea6b69d4227ed8296d712e09a28a33
Pirijan <hi@pirijan.com>
2025-05-09 12:23:20 -0400

    add railway.info query

--- src/app.js
+++ src/app.js
@@ -11,15 +11,18 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Serve static files from /public
-app.use(express.static('public'))
 
-// Routes
-app.get('/api', (request, response) => {
+// Server Routes
+app.get('/api/service', async (request, response) => {
+  const data = await railway.service()
   response.json({
     message: 'railway conductor',
+    data
   })
 })
+
+// Client Routes
+app.use(express.static('public'))
 app.get('/', (request, response) => {
   response.sendFile('./public/index.html', { root: '.' })
 })
@@ -30,7 +33,7 @@ app.use((error, request, response, next) => {
   response.status(500).json({ error: '(ノдヽ)' })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`🔮 Server running on port ${PORT}`)
 })
--- src/railway.js
+++ src/railway.js
@@ -1,8 +1,79 @@
+const apiHost = 'https://backboard.railway.com/graphql/v2'
+
 export default {
-  async listProjects () {
-
+  async railway (query) {
+    try {
+      const response = await fetch('https://backboard.railway.app/graphql/v2', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${process.env.RAILWAY_API_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query,
+          variables: {
+            environmentId: process.env.RAILWAY_ENVIRONMENT_ID,
+            serviceId: process.env.RAILWAY_SERVICE_ID
+          }
+        })
+      })
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error('🚒 railway', error, query)
+    }
   },
-  async deployProject () {
 
-  }
+  async service () {
+    try {
+      const query = `
+        query serviceInstance($environmentId: String!, $serviceId: String!) {
+          serviceInstance(environmentId: $environmentId, serviceId: $serviceId) {
+            __typename
+            buildCommand
+            builder
+            createdAt
+            cronSchedule
+            deletedAt
+            environmentId
+            healthcheckPath
+            healthcheckTimeout
+            id
+            isUpdatable
+            nextCronRunAt
+            nixpacksPlan
+            numReplicas
+            preDeployCommand
+            railpackInfo
+            railwayConfigFile
+            region
+            restartPolicyMaxRetries
+            restartPolicyType
+            rootDirectory
+            serviceId
+            serviceName
+            sleepApplication
+            startCommand
+            updatedAt
+            upstreamUrl
+            watchPatterns
+          }
+        }
+      `
+      return this.railway(query)
+      // console.log(response)
+      // if (!response.ok) {
+      //   throw new Error(`Failed to fetch service instance: ${response.status}`);
+      // }
+      // return data
+    } catch (error) {
+      throw error
+    }
+  },
+  async start () {
+// deploy
+  },
+  async stop () {
+// delete deployment
+  },
 }
